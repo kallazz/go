9cd3662f41628c31b6457db7f3d845945b44dbe0
kallazz <michael.w.kallas@gmail.com>
2023-12-16 16:44:48 +0100

    Added board

--- src/main/java/kallas/zubrzycki/IBoard.java
+++ src/main/java/kallas/zubrzycki/IBoard.java
@@ -3,4 +3,6 @@ package kallas.zubrzycki;
 public interface IBoard {
     public void printBoard();
     public void updateBoard(int x, int y, EPointColor state);
+    public void addObserverPlayer(EPointColor playerColor);
+    public void notifyPlayer(EPointColor playerColor);
 }
