2308f0a5af6543e37af758106b6b4a3145120905
kallazz <michael.w.kallas@gmail.com>
2023-12-16 17:17:41 +0100

    Added input parsing

--- src/main/java/kallas/zubrzycki/GameManager.java
+++ src/main/java/kallas/zubrzycki/GameManager.java
@@ -3,32 +3,64 @@ package kallas.zubrzycki;
 import java.util.Scanner;
 
 public class GameManager implements IGameManager {
-    
-
     private Board board;
     private Player player1;
     private Player player2;
-    private boolean isDoublePassed = false;
-    private boolean isPassed = false;
+    private static int BOARD_SIZE = 19;
+
+    private boolean isPassedPlayer1 = false;
+    private boolean isPassedPlayer2 = false;
+
     private Player currentPlayer = player1;
 
     @Override
     public void initializeGame() {
         player1 = new Player(EPointColor.BLACK);
         player2 = new Player(EPointColor.WHITE);
-        board = new Board(19);
+        board = new Board(BOARD_SIZE);
     }
 
     @Override
     public void beginGame() {
-        while (!isDoublePassed) { 
-            getInput();
+        while (!(isPassedPlayer1 && isPassedPlayer2)) { 
+            final String input = currentPlayer.readInput();
+
+            boolean isInputSuccessful = false;
+            do {
+                if (input.equals("pass")) {
+                    if (currentPlayer == player1) {
+                        isPassedPlayer1 = true;
+                    }
+                    else {
+                        isPassedPlayer2 = true;
+                    }
+                    isInputSuccessful = true;
+                }
+                else {
+                    final String[] words = input.split(" ");
+
+                    if (words[0].equals("go")) {
+                        try {
+                            final int x = Integer.parseInt(words[1]);
+                            final int y = Integer.parseInt(words[2]);
+
+                            if (x < 0 || x > BOARD_SIZE || y < 0 || y > BOARD_SIZE) {
+                                System.out.println("x and y should be in <1, " + BOARD_SIZE + ">");
+                            }
+                            else {
+                                isInputSuccessful = true;
+                            }
+                        }
+                        catch (Exception ex) {
+                            System.out.println("Correct format: go <x> <y>");
+                        }
+                    }
+                }
+            } while (!isInputSuccessful);
+
+            
 
         }
-    }
-
-    @Override
-    public void getInput() {
     }
 
     @Override
--- src/main/java/kallas/zubrzycki/IGameManager.java
+++ src/main/java/kallas/zubrzycki/IGameManager.java
@@ -3,6 +3,5 @@ package kallas.zubrzycki;
 public interface IGameManager {
     public void initializeGame();
     public void beginGame();
-    public void getInput();
     public void countScore();
 }
