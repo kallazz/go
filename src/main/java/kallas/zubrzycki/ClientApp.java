f8fb96cff076151c0a92c252143fcebe21390ff5
kallazz <michael.w.kallas@gmail.com>
2024-01-27 16:36:29 +0100

    Updated menu

--- src/main/java/kallas/zubrzycki/ClientApp.java
+++ src/main/java/kallas/zubrzycki/ClientApp.java
@@ -4,23 +4,32 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class ClientApp {
-    public static void main(String[] args) throws SQLException {
-        Client client = new Client();
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int input;
-        do {
-            System.out.println("1 - Join game. 2 - View Past Games");
-            input = scanner.nextInt();
-        } while(input == 1 || input == 0);
-        if(input == 1){
-            client.startConnection("127.0.0.1", 6666);
-            client.loop();
-        } else {
-            System.out.println("Here are all previous games:");
-            SQLLiteJDBC db = new SQLLiteJDBC("jdbc:sqlite:database.db");
 
+        System.out.println("1. Join Game\n2. View Past Games\n3. Exit");
+
+        String input = scanner.nextLine();
+        while (!input.equals("1") && !input.equals("2") && !input.equals("3")) {
+            System.out.println("Incorrect input. The only allowed options are 1, 2, 3.");
+            input = scanner.nextLine();
         }
 
-
+        if (input.equals("1")) {
+            Client client = new Client();
+            client.startConnection("127.0.0.1", 6666);
+            client.loop();
+        } else if (input.equals("2")) {
+            System.out.println("Here are all previous games:");
+            try {
+                SQLLiteJDBC db = new SQLLiteJDBC("jdbc:sqlite:database.db");
+            }
+            catch (SQLException ex) {
+                System.out.println("Something went wrong with the database");
+            }
+        } else {
+            scanner.close();
+            System.exit(0);
+        }
     }
 }
